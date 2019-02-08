e7382c01ec3a9574c7cc6dcaf1b2c3cca801f5ef
Sergio Ugalde <serch037@gmail.com>
2019-02-08 14:26:19 -0600

    Styling

--- main.c
+++ main.c
@@ -8,44 +8,58 @@
 #include "beto.h"
 #include "runner.h"
 
-int main () {
-   int input, data;
-   node_t *head = parse_csv();
-   do {
-      printf("1. Adding an item to the end of the list.\n");
-      printf("2. Iterating over a list.\n");
-      printf("3. Adding an item to the beginning of the list (pushing to the list).\n");
-      printf("4. Removing the first item (popping from the list).\n");
-      printf("5. Removing the last item of the list.\n");
-      printf("6. Removing a specific item. \n");
-      printf("7. Size of the linked List. \n");
-      printf("8. Get element of the linked list. \n");
-      printf("9. Clear the linked list. \n");
-      printf("10. Print the linked list.\n");
-      printf("11. Print the oldest person.\n");
-      printf("12. Print the youngest person.\n");
-      printf("13. print the average of ages.\n");
-      printf("14. print all names that start with a given letter.\n");
-      printf("0. Exit\n");
-     
-      scanf("%d", &input);
-      switch( input ){
-         case 1: printf("%s\n", );
-         case 2: printf("%s\n", );
-         case 3: printf("%s\n", );
-         case 4: printf("%s\n", );
-         case 5: printf("%s\n", );
-         case 6: printf("%s\n", );
-         case 7: printf("%s\n", );
-         case 8: printf("%s\n", );
-         case 9: printf("%s\n", );
-         case 10: printf("%s\n", );
-         case 11: printf("%s\n", );
-         case 12: printf("%s\n", );
-         case 13: printf("%s\n", );
-         case 14: printf("%s\n", );
-      }     
-   } while (input != 0);
+int main() {
+    int input, data;
+    node_t *head = parse_csv();
+    do {
+        printf("1. Adding an item to the end of the list.\n");
+        printf("2. Iterating over a list.\n");
+        printf("3. Adding an item to the beginning of the list (pushing to the list).\n");
+        printf("4. Removing the first item (popping from the list).\n");
+        printf("5. Removing the last item of the list.\n");
+        printf("6. Removing a specific item. \n");
+        printf("7. Size of the linked List. \n");
+        printf("8. Get element of the linked list. \n");
+        printf("9. Clear the linked list. \n");
+        printf("10. Print the linked list.\n");
+        printf("11. Print the oldest person.\n");
+        printf("12. Print the youngest person.\n");
+        printf("13. print the average of ages.\n");
+        printf("14. print all names that start with a given letter.\n");
+        printf("0. Exit\n");
 
-   return 0;
+        scanf("%d", &input);
+        switch (input) {
+            case 1:
+                printf("%s\n",);
+            case 2:
+                printf("%s\n",);
+            case 3:
+                printf("%s\n",);
+            case 4:
+                printf("%s\n",);
+            case 5:
+                printf("%s\n",);
+            case 6:
+                printf("%s\n",);
+            case 7:
+                printf("%s\n",);
+            case 8:
+                printf("%s\n",);
+            case 9:
+                printf("%s\n",);
+            case 10:
+                printf("%s\n",);
+            case 11:
+                printf("%s\n",);
+            case 12:
+                printf("%s\n",);
+            case 13:
+                printf("%s\n",);
+            case 14:
+                printf("%s\n",);
+        }
+    } while (input != 0);
+
+    return 0;
 }
